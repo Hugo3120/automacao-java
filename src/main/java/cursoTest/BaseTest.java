a1eac8f653e9f9d8ec885af1e23db6ab83894a4a
Hugo Leonardo <88748776+Hugo3120@users.noreply.github.com>
2021-10-26 10:18:35 -0300

    Testes em uma Loja Virtual vendo diferença entre "Assert" e "asserThat" e utilizando ListWebElement para testar um preço.

--- src/main/java/cursoTest/BaseTest.java
+++ src/main/java/cursoTest/BaseTest.java
@@ -20,4 +20,6 @@ public class BaseTest {
     public void fechaNavegador(){
         driver.quit();
     }
+
+
 }
--- src/main/java/cursoTest/LojaVirtualTest.java
+++ src/main/java/cursoTest/LojaVirtualTest.java
@@ -1,28 +1,61 @@
 package cursoTest;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+
+import org.junit.Assert;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 
 public class LojaVirtualTest extends BaseTest {
 
     @Test
     public void testPesquisaLivro() {
-        abreNavegador();
         WebElement pesquisa = driver.findElement(By.id("search"));
         pesquisa.sendKeys("fortaleza digital", Keys.ENTER);
         WebElement elTituloLivro = driver.findElement(By.xpath("//ul/li/h2/a"));
         String livro = elTituloLivro.getText();
-        Assertions.assertEquals("[PRODUTO DE EXEMPLO] - Fortaleza Digital",livro);
+        Assert.assertEquals("[PRODUTO DE EXEMPLO] - Fortaleza Digital", livro);
         WebElement elPreco = driver.findElement(By.cssSelector("#product-price-44 > span"));
         String preco = elPreco.getText();
-        Assertions.assertEquals("R$519,90", preco);
+        Assert.assertEquals("R$519,90", preco);
 
+    }
 
+    @Test
+    public void testPesquisaLivro_AssertThat() {
+        WebElement pesquisa = driver.findElement(By.id("search"));
+        pesquisa.sendKeys("fortaleza digital", Keys.ENTER);
+        WebElement elTituloLivro = driver.findElement(By.xpath("//ul/li/h2/a"));
+        String livro = elTituloLivro.getText();
+        Assert.assertEquals("[PRODUTO DE EXEMPLO] - Fortaleza Digital", livro);
+        WebElement elPreco = driver.findElement(By.cssSelector("#product-price-44 > span"));
+        String preco = elPreco.getText();
+        assertThat("R$519,90", is(preco));
+        assertThat("R$519,90", containsString(preco));
 
+    }
+
+    @Test
+    public void testClickLista() {
+        WebElement pesquisa = driver.findElement(By.id("search"));
+        pesquisa.sendKeys("html", Keys.ENTER);
+        List<WebElement> elLivros = driver.findElements(By.cssSelector("ul.products-grid > li"));
+        for (WebElement elLivro : elLivros) {
+            WebElement elTituloLivro = elLivro.findElement(By.cssSelector("h2 > a"));
+            String tituloLivro = elTituloLivro.getText();
+            if (tituloLivro.contains("Ajax com Java")) {
+                WebElement elpreco = elLivro.findElement(By.cssSelector("span.price"));
+                assertThat("R$444,50", is(elpreco.getText()));
+
+            }
+        }
 
     }
 
