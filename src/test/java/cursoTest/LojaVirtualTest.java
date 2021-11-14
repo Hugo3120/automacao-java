75b3f57f6c4107a4baab53a573f8614ca122144b
Hugo Leonardo <88748776+Hugo3120@users.noreply.github.com>
2021-11-14 13:17:28 -0300

    finalizando, esquema de cenario, tags e nova pagina

---
+++ src/test/java/cursoTest/LojaVirtualTest.java
@@ -0,0 +1,51 @@
+package cursoTest;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.Keys;
+
+import core.Driver;
+import pages.InicialPage;
+
+public class LojaVirtualTest extends BaseTest{
+	InicialPage inicialPage = new InicialPage();
+	@Test
+	public void testPesquisaLivro() {
+		Driver.setUrl("https://lojaexemplod.lojablindada.com");
+		inicialPage.setPesquisa("fortaleza digital", Keys.ENTER);
+		String livro = inicialPage.getTituloLivro();
+		Assert.assertEquals("[PRODUTO DE EXEMPLO] - Fortaleza Digital", livro);
+		String preco = inicialPage.getPreco();
+		Assert.assertEquals("R$519,90", preco);
+	}
+	
+	@Test
+	public void testPesquisaLivro_assertThat() {
+		Driver.setUrl("https://lojaexemplod.lojablindada.com");
+		inicialPage.setPesquisa("fortaleza digital", Keys.ENTER);
+		String livro = inicialPage.getTituloLivro();
+		Assert.assertEquals("[PRODUTO DE EXEMPLO] - Fortaleza Digital", livro);
+		String preco = inicialPage.getPreco();
+		assertThat("R$519,90", is(preco));
+		assertThat("R$519,90", containsString(preco));
+	}
+	
+	
+	@Test
+	public void testClickLista() {
+		Driver.setUrl("https://lojaexemplod.lojablindada.com");
+		inicialPage.setPesquisa("html", Keys.ENTER);
+		
+		String preco = inicialPage.getPrecoLista();
+		assertThat(null, is(preco));
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ src/test/java/steps/InicialSteps.java
@@ -0,0 +1,39 @@
+package steps;
+
+import static org.junit.Assert.assertEquals;
+
+import org.openqa.selenium.Keys;
+
+import core.Driver;
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+import pages.InicialPage;
+
+public class InicialSteps{
+	InicialPage inicialPage = new InicialPage();
+	@Dado("que estou na pagina loja virtual")
+	public void queEstouNaPaginaLojaVirtual() {
+	    Driver.setUrl("http://www.lojaexemplodelivros.com.br/");
+	}
+	
+	@Quando("realizo a busca do livro {string}")
+	public void realizoABuscaDoLivro(String titulo) {
+	    inicialPage.setPesquisa(titulo, Keys.ENTER);
+	}
+	@Entao("valido o titulo sendo {string}")
+	public void validoOTituloSendo(String titulo) {
+	    assertEquals(titulo, inicialPage.getTituloLivro());
+	}
+	@Entao("o preco sendo {string}")
+	public void oPrecoSendo(String preco) {
+	    assertEquals(preco, inicialPage.getPreco());
+	}
+	@Quando("clico no livro pesquisado")
+	public void clicoNoLivroPesquisado() {
+	   inicialPage.clickTitulo();
+	}
+	
+	
+	
+}
